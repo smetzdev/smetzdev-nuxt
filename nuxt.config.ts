b470f16fb28964e9a1141fbf5b333bcf826b9e7e
Sascha Metz <hello@smetz.dev>
2024-08-29 21:37:25 +0200

    refactor: :sparkles: add metadata and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          name: "description",
+          content: "Sascha Metz - Webdeveloper based in Saarbrücken, Germany",
+        },
+      ],
+      title: "Hello @smetz.dev",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
