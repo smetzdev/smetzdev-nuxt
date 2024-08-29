41d57918e62d5cb1376eb4408792995350825e7f
Sascha Metz <hello@smetz.dev>
2024-08-29 20:42:34 +0200

    refactor(components): :sparkles: add ButtonLink

---
+++ components/ButtonLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  href: String,
+});
+</script>
+
+<template>
+  <a
+    class="bg-brawn-yellow text-brawn-gray rounded-lg text-center px-4 py-2 ml-auto"
+    target="_blank"
+    :href="href"
+  >
+    <slot />
+  </a>
+</template>
