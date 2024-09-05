5f785dd398c9151e6d7c599a9f7c6f7fb1271f76
Sascha Metz <hello@smetz.dev>
2024-09-05 14:41:31 +0200

    refactor: :rocket: i hope you'll notice :wink:

--- components/CV.vue
+++ components/CV.vue
@@ -1,11 +1,12 @@
 <script setup>
-const showCV = ref(false);
-
 // Render component only if ?showCV is present in the url-search-params
+const showCV = ref(false);
 onMounted(() => {
   const urlParams = new URLSearchParams(window.location.search);
   showCV.value = urlParams.has("showCV");
 });
+
+const workplaces = await queryContent("workplaces").find();
 </script>
 
 <template>
@@ -19,41 +20,29 @@ onMounted(() => {
     <ul
       class="bg-gray-800 flex flex-col rounded-md mt-8 divide-y divide-gray-700 px-4 sm:px-8"
     >
-      <Workplace
-        company="Gorilla"
-        companyLink="https://gorilla.build/"
-        logo="/gorilla.svg"
-        job="Software Engineer"
-        yearsActive="2021 - today"
-      />
-      <Workplace
-        company="Holoux"
-        companyLink="https://hololux.de/"
-        logo="/hlx.svg"
-        job="Fullstack Web Developer"
-        yearsActive="2023 - today"
-      />
-      <Workplace
-        company="Browsaar"
-        companyLink="https://browsaar.de"
-        logo="/browsaar.svg"
-        job="CEO, CTO & Founder 😎"
-        yearsActive="2023 - today"
-      />
-      <Workplace
-        company="Azury Living"
-        companyLink="https://azury.de/"
-        logo="/azury.svg"
-        job="Fullstack Web Developer"
-        yearsActive="2020 - 2023"
-      />
-      <Workplace
-        company="raum3"
-        companyLink="https://raum3.de"
-        logo="/r3.svg"
-        job="Web Developer"
-        yearsActive="2015 - 2020"
-      />
+      <li
+        class="flex items-center justify-between py-4 sm:py-8"
+        v-for="workplace in workplaces"
+      >
+        <div class="sm:flex sm:items-center sm:space-x-4">
+          <img
+            :src="workplace.logo"
+            :alt="workplace.company"
+            class="w-12 h-12 rounded-lg max-sm:hidden"
+          />
+          <header>
+            <h3 class="sm:text-xl md:font-medium">
+              <a target="_blank" :href="workplace.companyLink">{{
+                workplace.company
+              }}</a>
+            </h3>
+            <p class="mt-1 text-gray-400 text-sm sm:text-base">
+              {{ workplace.job }}
+            </p>
+          </header>
+        </div>
+        <span class="text-sm sm:text-base">{{ workplace.yearsActive }}</span>
+      </li>
     </ul>
   </section>
 </template>
